9f216427dd980d622120b3e5045b4533500b8b6f
LucaCogo <lucacogo997@gmail.com>
2022-10-12 10:38:43 +0200

    Changes to run_demo.sh (long_seq)

--- SPyNet/run_demo.sh
+++ SPyNet/run_demo.sh
@@ -12,6 +12,9 @@ python run.py --model=sintel-final --path=../demo-frames-reds-lq/seq_2
 echo "REDS-LQ-SEQ_3"
 python run.py --model=sintel-final --path=../demo-frames-reds-lq/seq_3
 
+echo "REDS-LONG-LQ-SEQ"
+python run.py --model=sintel-final --path=../demo-frames-reds-lq/long_seq
+
 echo "REDS-HQ-SEQ_1"
 python run.py --model=sintel-final --path=../demo-frames-reds-hq/seq_1
 
@@ -20,3 +23,6 @@ python run.py --model=sintel-final --path=../demo-frames-reds-hq/seq_2
 
 echo "REDS-HQ-SEQ_3"
 python run.py --model=sintel-final --path=../demo-frames-reds-hq/seq_3
+
+echo "REDS-LONG-HQ-SEQ"
+python run.py --model=sintel-final --path=../demo-frames-reds-hq/long_seq
